71a00e1f1dab43ffedd4cef9255095c7ac8972bf
GOWTHAM S <144663893+GowthamS5@users.noreply.github.com>
2023-10-31 00:55:29 -0700

    Update SP.js

--- models/SP.js
+++ models/SP.js
@@ -1,31 +1,9 @@
-module.exports = {
-  createEmployee: {
-    name: 'CALL createem(?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    parameters: ['first_name', 'last_name', 'department', 'salary', 'DOB', 'email', 'password', 'role', 'imageFilePath'],
-  },
-  updateEmployee: {
-    name: 'CALL UpdateEmployee5(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    parameters: ['employeeId', 'first_name', 'last_name', 'email', 'password', 'role', 'department', 'salary', 'image', 'userRole'],
-  },
-  deleteEmployee: {
-    name: 'CALL Deleteem(?)',
-    parameters: ['employeeId'],
-  },
-  getEmployee: {
-    name: 'CALL Getem(?)',
-    parameters: ['employeeId'],
-  },
-  getEmployeeDetails: {
-    name: 'CALL GetEmployeeDetails(?, ?)',
-parameters :['userRole', 'employeeId'],
-  },
-  countEmployeesWithDOB: {
-    name: 'CALL CountEmployeesWithDOB(?)',
-    parameters: ['formattedDOB'],
-  },
- GetUserByEmail : {
-   name:'CALL GetUserByEmail(?)',
-   parameters:['email'],
-},
 
+module.exports = {
+  createEmployee: 'CALL createem(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+  updateEmployee: 'CALL UpdateEmployee5(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+  deleteEmployee: 'CALL Deleteem(?)',
+  getEmployeeDetails: 'CALL GetEmployeeDetails(?, ?)',
+  countEmployeesWithDOB: 'CALL CountEmployeesWithDOB(?)',
+  GetUserByEmail: 'CALL GetUserByEmail(?)',
 };
