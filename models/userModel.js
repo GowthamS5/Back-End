3e065f3868af1d4879efd6d911923e3fbdd572dc
GOWTHAM S <144663893+GowthamS5@users.noreply.github.com>
2023-10-31 00:16:14 -0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,10 @@
 const db = require('../config/database');
+const SP = require('./SP');
 
 class UserModel {
   static getUserByEmail(email) {
     return new Promise((resolve, reject) => {
-      const query = 'CALL GetUserByEmail(?)'; 
+      const query = SP.GetUserByEmail.name; 
       db.query(query, [email], (err, results) => {
         if (err) {
           reject(err);
