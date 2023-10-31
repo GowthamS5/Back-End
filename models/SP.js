ef89eca5b4ce83bed8aa6086876788a0b2711556
GOWTHAM S <144663893+GowthamS5@users.noreply.github.com>
2023-10-31 02:23:04 -0700

    Update SP.js

--- models/SP.js
+++ models/SP.js
@@ -3,6 +3,7 @@ module.exports = {
   createEmployee: 'CALL createem(?, ?, ?, ?, ?, ?, ?, ?, ?)',
   updateEmployee: 'CALL UpdateEmployee5(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
   deleteEmployee: 'CALL Deleteem(?)',
+  getEmployee: 'CALL Getem(?)',
   getEmployeeDetails: 'CALL GetEmployeeDetails(?, ?)',
   countEmployeesWithDOB: 'CALL CountEmployeesWithDOB(?)',
   GetUserByEmail: 'CALL GetUserByEmail(?)',
